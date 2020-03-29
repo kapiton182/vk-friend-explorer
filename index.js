02e18edcf907f3ca2c4de5169a584df7ed60abe4
kapitoshka <kapiton182@gmail.com>
2020-03-29 19:51:41 +0300

    test commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/sigh-in-form', (req, res) => {
+  res.render('pages/sigh-in-form', {
+    token: '1234515351'
+  })
+});
+
+app.get('/oauth-vk-callback', (req, res) => {
+  res.render('pages/callback-page', {
+    token: req.query.access_token
+  })
+})
+
+// https://oauth.vk.com/authorize?client_id=7380755&display=page&redirect_uri=https://vk-friend-explorer.herokuapp.com/oauth-vk-callback&scope=friends&response_type=token&v=5.103&state=123456
